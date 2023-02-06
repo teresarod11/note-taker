675227ec5a7af1fb0171af88a9498b96125d187e
teresarod11 <rodriguez49372@gmail.com>
2023-02-06 16:23:41 -0500

    updated server.js

--- Develop/server.js
+++ Develop/server.js
@@ -4,30 +4,34 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 // 
-const PORT = process.env.PORT || 3000;
+const PORT = 3005;
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => { 
-//     console.log('here');
-//     res.sendFile(path.join(__dirname, './public/index.html'))
-// });
+app.get('/', (req, res) => { 
+    console.log('here');
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
 
 app.get('/notes', (req, res) => {
     console.log('here');
     res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
-// app.get('/api/notes', (req, res) => {
-//     fs.readFile('./db/db.json')
-// });
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        res.json(JSON.parse(data))
+    });
+});
 
-// app.post('/api/notes', (req, res) => {
-//     res.readFile('./db/db.json')
-// });
+app.post('/api/notes', (req, res) => {
+    res.readFile('./db/db.json', (err, data) => {
+        
+    })
+});
 
 app.listen(PORT, () => {
     console.log(`Listening to ${PORT}`)
